d9eb4d935fd2bbcd9904da17117318af33f29d1d
TDCQCX <3290264220@qq.com>
2025-02-25 10:22:38 +0800

    修改

--- twikoo/config.js
+++
@@ -1,8 +0,0 @@
-// 在twikoo目录下创建config.js文件，并添加以下内容进行配置
-module.exports = {
-    api: 'https://api.github.com', // GitHub API地址
-    owner: 'TDCQCX', // 你的GitHub用户名
-    repo: 'TDCQCX.github.io', // 你的GitHub仓库名，用于存储评论数据
-    clientID: 'Ov23liULkcqwEsQoSLaE', // GitHub应用的Client ID
-    clientSecret: '585beabf62b2f67f0d9f960809c3b4f11562ce13' // GitHub应用的Client Secret
-};
